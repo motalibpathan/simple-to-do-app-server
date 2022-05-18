649eff9d834964e048d96e093a346c58e0291828
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-05-18 22:54:47 +0600

    insert task route added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hyb1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const taskCollection = client.db("simple_to_do_app").collection("tasks");
+
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Simple to do list server running!");
+});
+
+app.listen(port, () => {
+  console.log("Simple to do list server running on port ", port);
+});
